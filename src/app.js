80304d6d3cea51e0e16b0110d5ec67e4d59d8672
tomasturri <tomasturri14@gmail.com>
2024-02-14 16:52:54 -0300

    views

--- src/app.js
+++ src/app.js
@@ -1,21 +1,60 @@
+const PUERTO = 8080;
 const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
+const ProductManager = require('./dao/fs/ProductManager.js');
+const productsRouter = require('./routes/products.route.js');
+const cartsRouter = require('./routes/carts.route.js');
+const viewRouter = require('./routes/view.route.js');
+const path = require('path');
+const socket = require('socket.io');
+const { engine, create} = require('express-handlebars');
+const MessageModel = require('./dao/models/message.model.js');
+require('./database.js');
 
+const hbs = create({
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true
+  }
+});
+
+const manager = new ProductManager('./src/models/productos.json');
+
+// creamos el servidor
 const app = express();
-const PORT = 8080
-;
 
+// Configuramos para funcione la carpeta public
+app.use(express.static('./src/public'));
+
+//Middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Definir rutas para productos y carritos
-const productsRouter = require('./routes/products.router');
-const cartsRouter = require('./routes/carts.router');
+// rutas
+app.use('/api', productsRouter);
+app.use('/api', cartsRouter);
+app.use('/', viewRouter);
 
-app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter);
+hbs.handlebars.registerHelper('multiply', (a, b) => a * b);
 
-// Iniciar el servidor
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, './views'));
+
+const httpServer = app.listen(PUERTO, () => {
+  console.log('El servidor esta corriendo en el puerto ' + PUERTO);
+});
+
+// socket.io
+
+const io = new socket.Server(httpServer); //note creamos una instancia de socket
+
+
+// establecemos la conexión
+
+io.on('connection', (socket) => {
+  socket.on('message', async(data) => {
+    await MessageModel.create(data);
+    const messages = await MessageModel.find();
+    io.sockets.emit('messagesLogs', messages);
+  });
 });
